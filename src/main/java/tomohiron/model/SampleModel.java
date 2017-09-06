dda665f7a43e231bb939e4d5bca34ff356f606b3
Tomohiro Nakamura <naka.tomohiro@gmail.com>
2017-09-06 16:45:45 +0900

    working with jersey

---
+++ src/main/java/tomohiron/model/SampleModel.java
@@ -0,0 +1,18 @@
+package tomohiron.model;
+
+public class SampleModel {
+
+	private String str;
+
+	public SampleModel(final String str) {
+		this.str = str;
+	}
+
+	public String getStr() {
+		return str;
+	}
+
+	public void setStr(final String str) {
+		this.str = str;
+	}
+}
